7a8a652cd11d7bddd80068e2281d402ce59cc424
Rishikesh Gopal <152694330+rishi12145@users.noreply.github.com>
2026-08-17 01:56:03 +0530

    Time: 0 ms (100%), Space: 131.2 MB (93.11%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+
+        for(int i = 0; i < stones.size(); i++)
+        {
+            if(stones[i] % 3 == 0)
+            {
+                count0++;
+            }
+            else if(stones[i] % 3 == 1)
+            {
+                count1++;
+            }
+            else
+            {
+                count2++;
+            }
+        }
+
+        if(count0 % 2 == 0)
+        {
+            return count1 > 0 && count2 > 0;
+        }
+        else
+        {
+            return abs(count1 - count2) > 2;
+        }
+    }
+};
