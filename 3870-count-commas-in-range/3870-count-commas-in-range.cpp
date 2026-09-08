26fe83cc269c77ab23220b71b038db1cd174050d
Rishikesh Gopal <152694330+rishi12145@users.noreply.github.com>
2026-09-08 10:48:31 +0530

    Time: 0 ms (100%), Space: 8.5 MB (84.49%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int countCommas(int n) {
 
-        for(int i = 0; i <n; i++){
+        
             if(n > 999){
                 return n-999;
             }
             else{
                 return 0;
             }
-        }
+        
         return -1;
     }
 };
