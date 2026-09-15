be7bdb712199ceb0eda568a474d972540e790c2f
Rishikesh Gopal <152694330+rishi12145@users.noreply.github.com>
2026-09-15 23:55:10 +0530

    Time: 0 ms (100%), Space: 9.9 MB (46.89%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.cpp
+++ 0836-rectangle-overlap/0836-rectangle-overlap.cpp
@@ -1,10 +1,19 @@
-
 class Solution {
 public:
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
-        return rec1[0] < rec2[2] &&
-               rec2[0] < rec1[2] &&
-               rec1[1] < rec2[3] &&
-               rec2[1] < rec1[3];
+
+        int n = rec1.size();
+        int m = rec2.size();
+
+        if(n != m) {
+            return false;
+        }
+
+        if((rec1[0] < rec2[2] && rec1[2] > rec2[0]) &&
+           (rec1[1] < rec2[3] && rec1[3] > rec2[1])) {
+            return true;
+        }
+
+        return false;
     }
 };
