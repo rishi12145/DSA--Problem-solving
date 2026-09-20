c4162ceb21ac19fd27018f08776ed341e7f48de9
Rishikesh Gopal <152694330+rishi12145@users.noreply.github.com>
2026-09-20 19:49:05 +0530

    Time: 0 ms (100%), Space: 9.7 MB (78.4%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,15 @@
+    class Solution {
+    public:
+        int reverseDegree(string s) {
+
+            int ans = 0;
+
+            for(int i  = 0 ; i < s.size(); i++){
+                int value = 'z' - s[i] + 1;
+
+                ans = ans + value * (i+1);
+            }
+            return ans;
+            
+        }
+    };
